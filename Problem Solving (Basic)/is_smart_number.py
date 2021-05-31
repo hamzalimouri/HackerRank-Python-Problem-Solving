4e40f7a8070071756fc1e7011bc08be536829f87
Hamza LIMOURI <limouri.hamza@gmail.com>
2021-05-31 19:40:08 +0100

    Added Smart Number problem.

---
+++ Problem Solving (Basic)/is_smart_number.py
@@ -0,0 +1,17 @@
+import math
+
+
+def is_smart_number(num):
+    val = int(math.sqrt(num))
+    if math.sqrt(num) == val:
+        return True
+    return False
+
+
+for _ in range(int(input())):
+    num = int(input())
+    ans = is_smart_number(num)
+    if ans:
+        print("YES")
+    else:
+        print("NO")
